ea75a427507653468c75204e3e596df3e27a7c30
hauja47 <hoangletrunghau01@gmail.com>
2021-11-12 00:33:45 +0700

    Change Draw() and add DrawFlipX()

--- blaster-master/Sprite.cpp
+++ blaster-master/Sprite.cpp
@@ -9,10 +9,43 @@ Sprite::Sprite(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTU
 	this->right = right;
 	this->bottom = bottom;
 	this->texture = texture;
+
+	spriteHandler = Game::GetInstance()->GetSpriteHandler();
 }
 
-void Sprite::Draw(float x, float y)
+RECT Sprite::GetRect()
 {
-	Game* game = Game::GetInstance();
-	game->Draw(x, y, texture, left, top, right, bottom);
+	RECT r;
+
+	r.left = left;
+	r.right = right;
+	r.bottom = bottom;
+	r.top = top;
+
+	return r;
+}
+
+void Sprite::Draw(float x, float y, int alpha)
+{
+	D3DXVECTOR3 vector(x, y, 0);
+	RECT r = GetRect();
+	spriteHandler->Draw(texture, &r, NULL, &vector, D3DCOLOR_ARGB(alpha, 255, 255, 255));
+}
+
+void Sprite::DrawFlipX(float x, float y, int alpha)
+{
+	RECT r = GetRect();
+
+	D3DXMATRIX AA, BB;
+	spriteHandler->GetTransform(&AA);
+
+	D3DXVECTOR2 scalingCenter(x, x);
+	D3DXVECTOR2 scaling(-1.0f, 1.0f);
+	D3DXMatrixTransformation2D(&BB, &scalingCenter, 0.0f, &scaling, NULL, 0.0f, NULL);
+	D3DXMATRIX CC = AA * BB;
+	spriteHandler->SetTransform(&CC);
+	x -= abs(right - left);
+	D3DXVECTOR3 p(x, y, 0);
+	spriteHandler->Draw(texture, &r, NULL, &p, D3DCOLOR_ARGB(alpha, 255, 255, 255));
+	spriteHandler->SetTransform(&AA);
 }
--- blaster-master/Sprite.h
+++ blaster-master/Sprite.h
@@ -13,9 +13,13 @@ private:
 
 	LPDIRECT3DTEXTURE9 texture;
 
+	LPD3DXSPRITE spriteHandler;
+
 public:
 	Sprite(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTURE9 texture);
-	void Draw(float x, float y);
+	RECT GetRect();
+	void Draw(float x, float y, int alpha = 255);
+	void DrawFlipX(float x, float y, int alpha = 255);
 };
 
 typedef Sprite* LPSPRITE;
