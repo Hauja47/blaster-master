4f4e06a1c3d42b661d8bf380fcedb253368795fa
hauja47 <hoangletrunghau01@gmail.com>
2021-11-15 22:20:19 +0700

    Implement movement for Jason in top-down view

--- blaster-master/TopdownJason.cpp
+++ blaster-master/TopdownJason.cpp
@@ -1,28 +1,129 @@
 #include "TopdownJason.h"
+
 TopdownJason::TopdownJason(float x, float y)
 {
+	start_x = x;
+	start_y = y;
+
+	this->x = x;
+	this->y = y;
+
+	SetState(JASON_GO_RIGHT);
 }
+
 void TopdownJason::SetSpeedX(float vx)
 {
+	this->vx = vx;
 }
+
 void TopdownJason::SetSpeedY(float vy)
 {
+	this->vy = vy;
 }
+
 void TopdownJason::Update(DWORD dt)
 {
+	GameObject::Update(dt);
+
+	if (vx == 0 && vy == 0)
+	{
+		this->SetIdleState();
+		return;
+	}
+
+	x += dx;
+	y += dy;
 }
+
 void TopdownJason::Render()
 {
+	LPANIMATION animation = NULL;
+
+	switch (GetState())
+	{
+	case JASON_IDLE_UP:
+		animation = Animations::GetInstance()->Get(-00004);
+		break;
+	case JASON_IDLE_DOWN:
+		animation = Animations::GetInstance()->Get(-00003);
+		break;
+	case JASON_IDLE_LEFT:
+		animation = Animations::GetInstance()->Get(-00002);
+		break;
+	case JASON_IDLE_RIGHT:
+		animation = Animations::GetInstance()->Get(-00002);
+		animation->RenderFlipX(x, y);
+		return;
+	case JASON_GO_UP:
+		animation = Animations::GetInstance()->Get(00004);
+		break;
+	case JASON_GO_DOWN:
+		animation = Animations::GetInstance()->Get(00003);
+		break;
+	case JASON_GO_LEFT:
+		animation = Animations::GetInstance()->Get(00002);
+		break;
+	case JASON_GO_RIGHT:
+		animation = Animations::GetInstance()->Get(00002);
+		animation->RenderFlipX(x, y);
+		return;
+	default:
+		break;
+	}
+
+	animation->Render(x, y);
 }
+
 void TopdownJason::SetIdleState()
 {
+	switch (this->GetState())
+	{
+	case JASON_GO_UP:
+		this->SetState(JASON_IDLE_UP);
+		break;
+	case JASON_GO_DOWN:
+		this->SetState(JASON_IDLE_DOWN);
+		break;
+	case JASON_GO_LEFT:
+		this->SetState(JASON_IDLE_LEFT);
+		break;
+	case JASON_GO_RIGHT:
+		this->SetState(JASON_IDLE_RIGHT);
+		break;
+	default:
+		break;
+	}
 }
+
 void TopdownJason::SetState(int state)
 {
+	GameObject::SetState(state);
+
+	//switch (state)
+	//{
+	//case JASON_GO_UP:
+	//	vy = -JASON_SPEED;
+	//	break;
+	//case JASON_GO_DOWN:
+	//	vy = JASON_SPEED;
+	//	break;
+	//case JASON_GO_LEFT:
+	//	vx = -JASON_SPEED;
+	//	break;
+	//case JASON_GO_RIGHT:
+	//	vx = JASON_SPEED;
+	//	break;
+	//default:
+	//	vx = 0.0f;
+	//	vy = 0.0f;
+	//	break;
+	//}
 }
+
 void TopdownJason::Reset()
 {
 }
+
 void TopdownJason::GetBoundingBox(float& left, float& top, float& right, float& bottom)
 {
 }
--- blaster-master/TopdownKeyHandler.cpp
+++ blaster-master/TopdownKeyHandler.cpp
@@ -5,26 +5,26 @@ void TopdownKeyHandler::KeyState(BYTE* states)
 	Game* game = Game::GetInstance();
 	TopdownJason* jason = ((TopdownScene*)scence)->GetPlayer();
 
+	float vx = .0f, vy = .0f;
+
 	if (game->IsKeyDown(DIK_UP))
 	{
-		jason->SetSpeedY(-JASON_SPEED);
+		vy += -JASON_SPEED;
 	}
 	if (game->IsKeyDown(DIK_DOWN))
 	{
-		jason->SetSpeedY(JASON_SPEED);
+		vy += JASON_SPEED;
 	}
 	if (game->IsKeyDown(DIK_LEFT))
 	{
-		jason->SetSpeedX(-JASON_SPEED);
+		vx += -JASON_SPEED;
 	}
 	if (game->IsKeyDown(DIK_RIGHT))
 	{
-		jason->SetSpeedX(JASON_SPEED);
+		vx += JASON_SPEED;
 	}
-	else
-	{
-		jason->SetSpeed(0.0f, 0.0f);
-	}
+
+	jason->SetSpeed(vx, vy);
 }
 
 void TopdownKeyHandler::OnKeyDown(int KeyCode)
@@ -36,15 +36,23 @@ void TopdownKeyHandler::OnKeyDown(int KeyCode)
 	switch (KeyCode)
 	{
 	case DIK_UP:
+		if (jason->GetState() == JASON_GO_LEFT || jason->GetState() == JASON_GO_RIGHT)
+			return;
 		jason->SetState(JASON_GO_UP);
 		break;
 	case DIK_DOWN:
+		if (jason->GetState() == JASON_GO_LEFT || jason->GetState() == JASON_GO_RIGHT)
+			return;
 		jason->SetState(JASON_GO_DOWN);
 		break;
 	case DIK_LEFT:
+		if (jason->GetState() == JASON_GO_UP || jason->GetState() == JASON_GO_DOWN)
+			return;
 		jason->SetState(JASON_GO_LEFT);
 		break;
 	case DIK_RIGHT:
+		if (jason->GetState() == JASON_GO_UP || jason->GetState() == JASON_GO_DOWN)
+			return;
 		jason->SetState(JASON_GO_RIGHT);
 		break;
 	default:
